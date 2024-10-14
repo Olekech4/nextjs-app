6d72565dfdb04ed4e6ca959ef652bb507ccd2c04
olekech <olekech4@gmail.com>
2024-10-14 14:42:46 +0100

    nextjs

---
+++ my-app/src/app/componenet/useContext.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useContext } from 'react';
+import { useState } from 'react';
+
+const   ThemeContext = React.createContext();
+
+function App() {
+    const [theme, setTheme] = useState('light');
+
+    return (
+        <ThemeContext.provider value={{theme, setTheme}}>
+            <Toolbar />
+        </ThemeContext.provider>
+    );
+}
+
+function Toolbar() {
+    return (
+        <div>
+            <ThemeButton />
+        </div>
+    );
+}
+
+function ThemeButton() {
+    const [theme, setTheme] = useContext(ThemeContext);
+
+    return(
+        <button 
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        style={{background: theme === "light" ? "#fff" : "#333", color: theme === "light" ? "#000" : "#fff"}}>
+            Toggle THEME
+        </button>
+    )
+}
+export default ThemeContext
---
+++ my-app/src/app/page.js
@@ -0,0 +1,14 @@
+
+// import Counter from "componenet/counter";
+
+import Counter from "./componenet/counter";
+import UserList from "./componenet/useEffect";
+
+export default function Home() {
+  return (
+    <div>
+     {/* < Counter /> */}
+     <UserList/>
+    </div>
+  );
+}
